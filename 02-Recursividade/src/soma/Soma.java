7d0a91962529b20c55cfb85d4854a2eedd1dd8f4
1551933 <1551933@PAR462495.ifrn.local>
2026-04-14 16:14:01 -0300

    recursividade

--- 02-Recursividade/src/soma/Soma.java
+++ 02-Recursividade/src/soma/Soma.java
@@ -3,14 +3,14 @@ package soma;
 public class Soma {
 
     public static double recursivo(int n) {
-        if (n == 1)
-            return 1;
+        if (n == 0)
+            return 0;
         else
             return n + recursivo(n-1);
     }
     public static double iterativo(int n) {
-        double resultado = 1;
-        for (int i=2; i<=n; i++)
+        double resultado = 0;
+        for (int i=1; i<=n; i++)
             resultado += i;
         return resultado;
     }
