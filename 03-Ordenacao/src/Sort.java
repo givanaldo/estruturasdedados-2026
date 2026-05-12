0a092cf137b91b08f847c2a1bdce76c973bb9e58
1551933 <1551933@PAR462495.ifrn.local>
2026-05-12 16:34:41 -0300

    ordenação bubblesort

--- 03-Ordenacao/src/Sort.java
+++ 03-Ordenacao/src/Sort.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class Sort {
     public static void bubblesort(int[] v) {
         int n = v.length;
@@ -8,7 +11,23 @@ public class Sort {
                     v[j] = v[j+1];
                     v[j+1] = aux;
                 }
+                System.out.println(Arrays.toString(v)); // passo-a-passo
             }
         }
     }
+    
+    public static void selectionsort(int[] v) {
+        int n = v.length;
+        for (int i=0; i <= n-2; i++) {
+            for (int j=i+1; j <= n-1; j++) {
+                if (v[i] > v[j]) {
+                    int aux = v[j];
+                    v[j] = v[i];
+                    v[i] = aux;
+                }
+                // Mostra passo-a-passo
+                System.out.println(Arrays.toString(v)); 
+            }
+        }
+    }    
 }
--- 03-Ordenacao/src/TestesSimples.java
+++ 03-Ordenacao/src/TestesSimples.java
@@ -1,6 +1,7 @@
 
 public class TestesSimples {
-    public static void imprimirVetor(int [] v) {
+    public static void imprimirVetor(String texto, int [] v) {
+        System.out.print(texto);
         for (int i = 0; i < v.length; i++) {
             System.out.print(v[i] + " ");
         }
@@ -8,10 +9,17 @@ public class TestesSimples {
     }
 
     public static void main(String[] args) {
-        int[] vetor = {5, 4, 2, 3, 1};
-        imprimirVetor(vetor);
-        Sort.bubblesort(vetor);
-        imprimirVetor(vetor);
-
+        int[] vetor1 = {5, 10, 4, 2, 3, 1, 23};
+        int[] vetor2 = vetor1.clone();
+        
+        imprimirVetor("Original = ", vetor1);
+        Sort.bubblesort(vetor1);
+        imprimirVetor("Ordenado = ", vetor1);
+        
+        imprimirVetor("Original = ", vetor2);
+        Sort.selectionsort(vetor2);
+        imprimirVetor("Ordenado = ", vetor2);
+        
+        
     }
 }
