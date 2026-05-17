65e6006bcf92738db1c6947370f6e6b0dc3458cc
Givanaldo <givanaldo@gmail.com>
2026-05-17 15:59:21 -0300

    ordenação

--- 03-Ordenacao/src/TestesSimplesTempo.java
+++ 03-Ordenacao/src/TestesSimplesTempo.java
@@ -1,7 +1,7 @@
 
 import java.util.Random;
 
-public class TestesTempoCArga {
+public class TestesSimplesTempo {
     public static void imprimirVetor(String texto, int [] v) {
         System.out.print(texto);
         for (int i = 0; i < v.length; i++)
@@ -10,52 +10,42 @@ public class TestesTempoCArga {
     }
 
     public static void main(String[] args) {
-        int n = 1000;
+        int n = 10000;
         long inicio, fim;
         Random numero = new Random();
         
         int[] vetor1 = new int[n];
         for (int i = 0; i < vetor1.length; i++)
-            vetor1[i] = numero.nextInt(1000);
+            vetor1[i] = numero.nextInt(10000);
         
         int[] vetor2 = vetor1.clone();
         int[] vetor3 = vetor1.clone();
         int[] vetor4 = vetor1.clone();
         int[] vetor5 = vetor1.clone();
         
-        imprimirVetor("Original = ", vetor1);
         inicio = System.currentTimeMillis();
         Sort.bubblesort(vetor1);
         fim = System.currentTimeMillis();
-        imprimirVetor("Bubble = ", vetor1);
-        System.out.println("Tempo: " + (fim - inicio) + " milisegundos\n");
+        System.out.println("Bubble sort: " + (fim - inicio) + " milisegundos");
         
-        imprimirVetor("Original = ", vetor2);
         inicio = System.currentTimeMillis();
         Sort.selectionsort(vetor2);
         fim = System.currentTimeMillis();
-        imprimirVetor("Selection = ", vetor2);
-        System.out.println("Tempo: " + (fim - inicio) + " milisegundos\n");
+        System.out.println("Selection sort: " + (fim - inicio) + " milisegundos");
         
-        imprimirVetor("Original = ", vetor3);
         inicio = System.currentTimeMillis();
         Sort.insertionsort(vetor3);
         fim = System.currentTimeMillis();
-        imprimirVetor("Insertion = ", vetor3);
-        System.out.println("Tempo: " + (fim - inicio) + " milisegundos\n");
+        System.out.println("Insertion sort: " + (fim - inicio) + " milisegundos");
 
-        imprimirVetor("Original = ", vetor4);
         inicio = System.currentTimeMillis();
         Sort.shellsort(vetor4);
         fim = System.currentTimeMillis();
-        imprimirVetor("Shell orig = ", vetor4);
-        System.out.println("Tempo: " + (fim - inicio) + " milisegundos\n");
+        System.out.println("Shell sort original: " + (fim - inicio) + " milisegundos");
 
-        imprimirVetor("Original = ", vetor5);
         inicio = System.currentTimeMillis();
         Sort.shellsort2(vetor5);
         fim = System.currentTimeMillis();
-        imprimirVetor("Shell = ", vetor5);
-        System.out.println("Tempo: " + (fim - inicio) + " milisegundos\n");        
+        System.out.println("Shell sort gap otimizado: " + (fim - inicio) + " milisegundos");        
     }
 }
