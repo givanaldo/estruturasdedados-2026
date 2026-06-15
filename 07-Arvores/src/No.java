342495405abcc21c917bf4053e0bdaaa13bec0fa
1551933 <1551933@PAR462495.ifrn.local>
2026-06-15 17:52:23 -0300

    Árvores

---
+++ 07-Arvores/src/No.java
@@ -0,0 +1,13 @@
+public class No {
+    private int valor;
+    private No esq;
+    private No dir;
+
+    public No(int valor) {
+        this.valor = valor;
+        this.esq = null;
+        this.dir = null;
+    }
+    
+    
+}
