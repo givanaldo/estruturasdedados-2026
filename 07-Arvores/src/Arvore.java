d56d67cf2e7af10549342cf4d64dc921286a5b84
1551933 <1551933@PAR462495.ifrn.local>
2026-06-16 15:38:14 -0300

    Árvores

---
+++ 07-Arvores/src/Arvore.java
@@ -0,0 +1,30 @@
+public class Arvore {
+    private No raiz;
+
+    public Arvore(No raiz) {
+        this.raiz = raiz;
+    }
+    
+    public boolean isEmpty() {
+        return this.raiz == null;
+    }
+    
+    public void inserir(No pai, No filho) {
+        if (this.isEmpty())
+            this.raiz = filho;
+        else {
+            if (filho.getValor() < pai.getValor()) {
+                if (pai.getEsq() == null)
+                    pai.setEsq(filho);
+                else
+                    inserir(pai.getEsq(), filho);
+            }
+            else{
+                if (pai.getDir() == null)
+                    pai.setDir(filho);
+                else
+                    inserir(pai.getDir(), filho);
+            }
+        }
+    }
+}
--- 07-Arvores/src/No.java
+++ 07-Arvores/src/No.java
@@ -8,6 +8,28 @@ public class No {
         this.esq = null;
         this.dir = null;
     }
-    
-    
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public No getEsq() {
+        return esq;
+    }
+
+    public void setEsq(No esq) {
+        this.esq = esq;
+    }
+
+    public No getDir() {
+        return dir;
+    }
+
+    public void setDir(No dir) {
+        this.dir = dir;
+    }    
 }
